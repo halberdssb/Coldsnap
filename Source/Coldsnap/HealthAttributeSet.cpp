7c8edc4a726bed4458c697a789d8e6f3654d5fa3
halberdssb <jwstevenson22@gmail.com>
2025-10-25 11:13:16 -0400

    Test Enemy Health Initialize Fixed

--- Source/Coldsnap/HealthAttributeSet.cpp
+++ Source/Coldsnap/HealthAttributeSet.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "GameplayEffectExtension.h"
 #include "HealthAttributeSet.h"
+#include "GameplayEffectExtension.h"
 
 /*
  * GAS Attribute Set for health
