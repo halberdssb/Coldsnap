b0ab139f8a91e25fd660b3f6cda03e9e022000b7
halberdssb <jwstevenson22@gmail.com>
2025-10-26 19:59:01 -0400

    Hitbox Custom Editor Work Started

---
+++ Source/Coldsnap/Public/HitboxInfo.h
@@ -0,0 +1,18 @@
+// Copyright 2025 Icebreak Studios. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * Handles data for hitboxes
+ *
+ * Jeff Stevenson
+ * 10.25.25
+ */
+struct COLDSNAP_API HitboxInfo
+{
+public:
+	HitboxInfo();
+	~HitboxInfo();
+};
