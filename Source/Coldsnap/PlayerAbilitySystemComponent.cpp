c7843e15c686a861c7b1eb151fac7d8e16989630
halberdssb <jwstevenson22@gmail.com>
2026-01-18 15:44:48 -0500

    Game Instance GAS Persistence Work

--- Source/Coldsnap/PlayerAbilitySystemComponent.cpp
+++ Source/Coldsnap/PlayerAbilitySystemComponent.cpp
@@ -12,5 +12,16 @@
 
 UPlayerAbilitySystemComponent::UPlayerAbilitySystemComponent()
 {
-	
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UPlayerAbilitySystemComponent::AddGameplayEffectToAppliedEffectsArray);
+}
+
+TArray<FGameplayEffectSpec> UPlayerAbilitySystemComponent::GetAllAppliedGameplayEffectSpecs()
+{
+	return AppliedGameplayEffects;
+}
+
+void UPlayerAbilitySystemComponent::AddGameplayEffectToAppliedEffectsArray(UAbilitySystemComponent* InASC, const FGameplayEffectSpec& InEffectSpec,
+                                                                           FActiveGameplayEffectHandle InEffectHandle)
+{
+	AppliedGameplayEffects.Add(InEffectSpec);
 }
---
+++ Source/Coldsnap/Public/GASDataPersistenceHandler.h
@@ -0,0 +1,41 @@
+// Copyright 2025 Icebreak Studios. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "COLDSNAPGameInstance.h"
+#include "Coldsnap/PlayerAbilitySystemComponent.h"
+#include "Components/ActorComponent.h"
+#include "GASDataPersistenceHandler.generated.h"
+
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class COLDSNAP_API UGASDataPersistenceHandler : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGASDataPersistenceHandler();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Data Persistence")
+	bool SaveGASDataToGameInstance();
+
+	UFUNCTION(BlueprintCallable, Category = "Data Persistence")
+	bool LoadGASDataFromGameInstance();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = General)
+	TObjectPtr<AActor> Player;
+
+	UPlayerAbilitySystemComponent* PlayerAbilitySystemComponent;
+private:	
+
+	UCOLDSNAPGameInstance* GameInstance;
+};
